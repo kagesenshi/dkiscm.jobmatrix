96547d48c0eca4340dcee22dc6ca30ff0e0c4121
Izhar Firdaus <izhar@inigo-tech.com>
2013-05-04 18:48:32 +0800

    fix indexer

--- dkiscm/jobmatrix/indexer.py
+++ dkiscm/jobmatrix/indexer.py
@@ -1,9 +1,14 @@
 from plone.indexer import indexer
 from dkiscm.jobmatrix.content.job import IJob
 from dkiscm.jobmatrix.content.industrycluster import IIndustryCluster
+from dkiscm.jobmatrix.content.jobgroup import IJobGroup
 
 @indexer(IJob)
 def job_industry_cluster(obj):
     if IIndustryCluster.providedBy(obj.__parent__):
         return obj.__parent__.id
+    if IJobGroup.providedBy(obj.__parent__):
+        jobgroup = obj.__parent__
+        if IIndustryCluster.providedBy(jobgroup.__parent__):
+            return jobgroup.__parent__.id
     return 'no-industry-cluster'
