109a68c6c6baed396670b0d4ff6e8f6efceb05a9
Izhar Firdaus <izhar@inigo-tech.com>
2013-10-24 11:05:36 +0800

    dont display download pdf link on frontpage

--- dkiscm/jobmatrix/viewlet/downloadpdf.py
+++ dkiscm/jobmatrix/viewlet/downloadpdf.py
@@ -5,11 +5,12 @@ from zope.component import getMultiAdapter
 from Products.CMFCore.interfaces import IContentish
 from plone.app.layout.viewlets import interfaces as manager
 from dkiscm.jobmatrix.interfaces import IProductSpecific
+from dkiscm.jobmatrix.content.job import IJob
 
 grok.templatedir('templates')
 
 class DownloadPDF(grok.Viewlet):
-    grok.context(IContentish)
+    grok.context(IJob)
     grok.viewletmanager(manager.IAboveContent)
     grok.template('downloadpdf')
     grok.layer(IProductSpecific)
